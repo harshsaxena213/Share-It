ff35823ccac80fbd4efa0a26ad5ebc35b6a82710
Harshit Saxena <dev.harshit@proton.me>
2025-08-02 03:17:46 +0530

    Connected Register Page To Backend

---
+++ include/login.h
@@ -0,0 +1,5 @@
+#include <crow.h>
+
+bool authentication(std::string username,std::string password){
+    return true;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <crow.h>
+#include "include/login.h"
 
 int main(){
     crow::SimpleApp app;
@@ -16,6 +17,28 @@ int main(){
     CROW_ROUTE(app,"/register")([](){
         auto load_page=crow::mustache::load("register.html");
         return load_page.render();
+    });
+
+    CROW_ROUTE(app,"/register_data").methods(crow::HTTPMethod::POST)([](crow::request& req,crow::response& res){
+        auto parameters=req.get_body_params();
+        auto username=parameters.get("username");
+        auto password=parameters.get("password");
+        bool status=authentication(username,password);
+        if(status){
+                    auto load_page=crow::mustache::load("confirmation.html");
+                    crow::mustache::context ctx;
+                    ctx={{"status","Registered!"}};
+                    std::string page_html = load_page.render(ctx).dump();
+                    res.set_header("Content-Type", "text/html");
+                    res.write(page_html);
+                    res.end();
+        }
+        else{
+                    res.code=404;
+                    res.body="Wrong Password";
+                    res.end();
+        }
+
     });
 
     CROW_ROUTE(app,"/upload")([](){
